ad17619da532aa971ce013bf3f0d06d3926c2985
ZohHub <rendonsterz@gmail.com>
2024-09-25 10:26:32 +0800

    Update ZHubRemakeV1.lua

--- ZHubRemakeV1.lua
+++ ZHubRemakeV1.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/ZohHub/ZohHub/refs/heads/ZHub/TEST")
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ZohHub/ZohHub/refs/heads/ZHub/TEST"))()
